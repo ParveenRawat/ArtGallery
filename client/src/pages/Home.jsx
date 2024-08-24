3962a3cedcedef5c04cd28f2ad699f037de34a0a
Prerak1310 <preraksangwan@gmail.com>
2024-08-24 18:16:13 +0530

    added art placards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import Card from "../components/Card";
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <section className="relative grid grid-cols-3">
+        <Card
+          srcurl="https://picsum.photos/id/237/200/300"
+          subtitle="The Subtitle"
+          title="Saitama"
+          desc="Just your favorite strongest anime character :)"
+        />
+        <Card
+          srcurl="https://dummyimage.com/600x360"
+          subtitle="The Subtitle"
+          title="Saitama"
+          desc="Just your favorite strongest anime character :)"
+        />
+        <Card
+          srcurl="https://dummyimage.com/600x360"
+          subtitle="The Subtitle"
+          title="Saitama"
+          desc="Just your favorite strongest anime character :)"
+        />
+        <Card
+          srcurl="https://dummyimage.com/600x360"
+          subtitle="The Subtitle"
+          title="Saitama"
+          desc="Just your favorite strongest anime character :)"
+        />
+        <Card
+          srcurl="https://dummyimage.com/600x360"
+          subtitle="The Subtitle"
+          title="Saitama"
+          desc="Just your favorite strongest anime character :)"
+        />
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;

302c2438133665369249b9e3924bc780d1c71b14
Prerak1310 <preraksangwan@gmail.com>
2024-08-24 23:50:30 +0530

    art placard finishing touches

--- client/src/components/ArtCard/Card.jsx
+++ client/src/components/ArtCard/Card.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Card({ srcurl, subtitle, title, desc }) {
   return (
     <>
-      <div className="relative m-5 h-96 p-2">
+      <div className="relative h-96 items-center justify-center p-2 m-5" >
         <img
           alt="gallery"
-          className="absolute h-full w-full object-cover object-center"
+          className="absolute inset-0 h-full w-full rounded-2xl object-cover shadow-md shadow-black"
           src={srcurl}
         />
-        <div className="absolute h-full w-full border-4 border-gray-200 bg-white px-8 py-10 opacity-0 hover:opacity-80">
+        <div className="inset-0 absolute h-full w-full border-4 border-gray-200 bg-white object-cover px-8 py-10 opacity-0 hover:opacity-80 rounded-2xl">
           <h2 className="title-font mb-1 text-sm font-medium tracking-widest text-indigo-700">
             {subtitle}
           </h2>

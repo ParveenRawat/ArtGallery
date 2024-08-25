eb2425412022b1a46c8cd53724b5c9fb10249b94
Prerak1310 <preraksangwan@gmail.com>
2024-08-25 14:16:46 +0530

    artcard improved

--- client/src/components/ArtCard/Card.jsx
+++ client/src/components/ArtCard/Card.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { AiFillHeart } from "react-icons/ai";
+import { FaRegComment } from "react-icons/fa6";
+import Like from "../CardButtons/Like";
 function Card({ srcurl, subtitle, title, desc }) {
   return (
     <>
-      <div className="relative h-96 items-center justify-center p-2 m-5" >
+      <div className="relative m-5 h-96 items-center justify-center p-2">
         <img
           alt="gallery"
           className="absolute inset-0 h-full w-full rounded-2xl object-cover shadow-md shadow-black"
           src={srcurl}
         />
-        <div className="inset-0 absolute h-full w-full border-4 border-gray-200 bg-white object-cover px-8 py-10 opacity-0 hover:opacity-80 rounded-2xl">
+        <div className="absolute inset-0 h-full w-full rounded-2xl border-4 border-gray-200 bg-white object-cover px-8 py-10 opacity-0 hover:opacity-80">
           <h2 className="title-font mb-1 text-sm font-medium tracking-widest text-indigo-700">
             {subtitle}
           </h2>
@@ -17,6 +20,14 @@ function Card({ srcurl, subtitle, title, desc }) {
             {title}
           </h1>
           <p className="leading-relaxed">{desc}</p>
+        </div>
+        <div className="absolute right-0 grid grid-rows-3 gap-2 p-2">
+          <Link to="/login">
+            <Like />
+          </Link>
+          <Link to="/login">
+            <FaRegComment className="size-6" />
+          </Link>
         </div>
       </div>
     </>
---
+++ client/src/components/CardButtons/Like.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { AiFillHeart } from "react-icons/ai";
+// const [liked, setLiked] = useState(false);
+// const handleClick = () => {
+//   setLiked(!liked);
+// };
+function Like() {
+  return 1==2?(<div>Hello</div>):(<AiFillHeart color="#ffffff" className="size-7"/>);
+}
+
+export default Like;
